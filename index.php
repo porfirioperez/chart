f7dedc3503ea5824ee32b2261f6f43269b82615d
Richard Perez <RichardPerez@Richards-MacBook-Pro.local>
2016-11-18 21:55:30 -0800

    added skeleton html code

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<head>
+  <meta charset="UTF-8">
+  <title>PasteChart</title>
+</head>
+<body>
 <?php
   session_start();  // start/continue the session
-
-  
-?>
-
+ 
+?>  
+</body>
 </html>
